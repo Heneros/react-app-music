299a3f973c17ab2fc41c42caab4b82e37355f745
Heneros <punk60826@gmail.com>
2022-08-24 02:22:38 +0300

    styles/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Grid, AppBar, Toolbar, Typography } from '@mui/material';
 import { HeadsetTwoTone } from '@mui/icons-material';
+import HeaderStyles from './styles/Header';
+
 
 function Header() {
     return (
-        <AppBar>
-            <Toolbar>
-                <HeadsetTwoTone />
-                <Typography variant="h6" component="h1">
-                    React App Music
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <HeaderStyles>
+            <AppBar color="secondary" position="fixed">
+                <Toolbar>
+                    <HeadsetTwoTone />
+                    <Typography className="title" variant="h6" component="h1">
+                        React App Music
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </HeaderStyles>
     )
 }
 
---
+++ src/components/styles/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+
+const HeaderStyles = styled.header`
+.title{
+    margin-left: 8px;
+}
+`;
+
+export default HeaderStyles;
