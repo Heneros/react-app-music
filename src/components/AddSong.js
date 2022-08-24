3c0b76c62878d1d140d86f32cf43a49f093397bd
Heneros <punk60826@gmail.com>
2022-08-25 02:16:56 +0300

    button, textfield and dialog created

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,51 @@
 import React from 'react';
-
+import { TextField, InputAdornment, Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { AddBoxOutlined, Link } from '@mui/icons-material';
 function AddSong() {
-    return <div>AddSong</div>
+    const [dialog, setDialog] = React.useState(false);
+
+
+    function handleSetDialog() {
+        setDialog(false);
+    }
+
+    return (
+        <div>
+            <Dialog
+                open={dialog}
+                onClose={handleSetDialog}
+            >
+                <DialogTitle>Edit Song</DialogTitle>
+                <DialogContent>
+                   <img src="https://avatars.githubusercontent.com/u/25700704?v=4" alt="Song thumbnail" />
+                   <TextField
+                   margin="dense"
+                   name="title"
+                   label="Title"
+                   fullWidth
+                   />
+                </DialogContent>
+            </Dialog>
+            <TextField
+                placeholder='Add Youtube or Soundcloud'
+                fullWidth
+                margin="normal"
+                type="url"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <Link />
+                        </InputAdornment>
+                    )
+                }}
+            />
+            <Button
+                onClick={() => setDialog(true)}
+                variant='contained' color="primary" endIcon={<AddBoxOutlined />}>
+                Add
+            </Button>
+        </div>
+    )
 }
 
 export default AddSong;
