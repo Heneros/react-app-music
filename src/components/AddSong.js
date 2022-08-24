00915080e1296bb0de31cbaf309d1f47b2651afd
Heneros <punk60826@gmail.com>
2022-08-25 02:51:40 +0300

    AddSongStyles.js

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,49 +1,97 @@
 import React from 'react';
-import { TextField, InputAdornment, Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { TextField, InputAdornment, Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material';
 import { AddBoxOutlined, Link } from '@mui/icons-material';
+import AddSongStyles from './styles/AddSongStyles';
+
+import { createGlobalStyle } from 'styled-components';
+
+
+
+
+const GlobalStyle = createGlobalStyle`
+  .thumbnail{
+    width: 90%;
+  }
+  .dialog{
+    text-align: center;
+}
+`
+
 function AddSong() {
     const [dialog, setDialog] = React.useState(false);
 
 
-    function handleSetDialog() {
+    function handleCloseDialog() {
         setDialog(false);
     }
 
     return (
         <div>
-            <Dialog
-                open={dialog}
-                onClose={handleSetDialog}
-            >
-                <DialogTitle>Edit Song</DialogTitle>
-                <DialogContent>
-                   <img src="https://avatars.githubusercontent.com/u/25700704?v=4" alt="Song thumbnail" />
-                   <TextField
-                   margin="dense"
-                   name="title"
-                   label="Title"
-                   fullWidth
-                   />
-                </DialogContent>
-            </Dialog>
-            <TextField
-                placeholder='Add Youtube or Soundcloud'
-                fullWidth
-                margin="normal"
-                type="url"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment>
-                            <Link />
-                        </InputAdornment>
-                    )
-                }}
-            />
-            <Button
-                onClick={() => setDialog(true)}
-                variant='contained' color="primary" endIcon={<AddBoxOutlined />}>
-                Add
-            </Button>
+            <GlobalStyle />
+            <AddSongStyles>
+                <div className='container'>
+                    <Dialog
+                        className='dialog'
+                        open={dialog}
+                        onClose={handleCloseDialog}
+                    >
+                        <DialogTitle>Edit Song</DialogTitle>
+                        <DialogContent>
+
+                            <img
+                                className='thumbnail'
+                                src="https://avatars.githubusercontent.com/u/25700704?v=4" alt="Song thumbnail" />
+
+                            <TextField
+                                margin="dense"
+                                name="title"
+                                label="Title"
+                                fullWidth
+                            />
+                            <TextField
+                                margin="dense"
+                                name="artist"
+                                label="Artist"
+                                fullWidth
+                            />
+                            <TextField
+                                margin="dense"
+                                name="thumbnail"
+                                label="Thumbnail"
+                                fullWidth
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleCloseDialog} color="secondary">
+                                Cancel
+                            </Button>
+                            <Button variant="outlined" color="primary">
+                                Add Song
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                    <TextField
+                        className='url-input'
+                        placeholder='Add Youtube or Soundcloud'
+                        fullWidth
+                        margin="normal"
+                        type="url"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Link />
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                    <Button
+                        className='addSongButton'
+                        onClick={() => setDialog(true)}
+                        variant='contained' color="primary" endIcon={<AddBoxOutlined />}>
+                        Add
+                    </Button>
+                </div>
+            </AddSongStyles>
         </div>
     )
 }
---
+++ src/components/styles/AddSongStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+const AddSongStyles = styled.div`
+.container{
+    display: flex;
+    align-items: center;
+
+}
+
+.url-input{
+
+}
+.addSongButton{
+    margin-left: 25px;
+}
+.dialog{
+    text-align: center;
+}
+
+`
+
+export default AddSongStyles;
