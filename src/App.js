306994156b892ca30a4f362a1d3d4ebbc14ee9e5
Heneros <punk60826@gmail.com>
2022-08-31 02:06:46 +0300

    media queries added to QueuedSongList

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import AddSong from './components/AddSong';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
 import { Grid } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
 
 function App() {
+  const theme = useTheme();
+
+  const greaterThanMd = useMediaQuery(theme.breakpoints.up('md'));
   return (
     <>
-    
+
       <Header />
       <Grid container spacing={3}>
         <Grid
@@ -18,12 +24,18 @@ function App() {
           <SongList />
         </Grid>
         <Grid
-          style={{
-            position: 'fixed',
-            width: '100%',
-            right: 0,
-            top: 70
-          }}
+          style={
+            greaterThanMd ? {
+              position: 'fixed',
+              width: '100%',
+              right: 0,
+              top: 70
+            } : {
+              position: 'fixed',
+              width: '100%',
+              left: 0,
+              bottom: 0
+            }}
           item xs={12} md={5}>
           <SongPlayer />
         </Grid>
