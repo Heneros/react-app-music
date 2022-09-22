4910f266b3b13e683960b5e3d9809cba43e856eb
Heneros <punk60826@gmail.com>
2022-09-23 02:24:25 +0300

    songPlayer component

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -6,18 +6,35 @@ import { SongContext } from '../App';
 import { GET_QUEUED_SONGS } from '../graphql/queries';
 import QueuedSongList from './QueuedSongList';
 import GlobalStyle from './styles/GlobalStyles';
-
+import ReactPlayer from 'react-player';
 
 function SongPlayer() {
-    // useQuery
     const { data } = useQuery(GET_QUEUED_SONGS);
-
-    // console.log( data );
-
     const { state, dispatch } = React.useContext(SongContext);
+    const [played, setPlayed] = React.useState(0);
+    const reactPlayerRef = React.useRef();
+
+
+    const [seeking, setSeeking] = React.useState(false);
+
+
 
     function handleTogglePlay() {
         dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
+    }
+
+    function handleProgressChange(event, newValue) {
+        setPlayed(newValue)
+    }
+
+    function handleSeekMouseDown() {
+        setSeeking(true);
+
+    }
+
+    function handleSeekMouseUp() {
+        setSeeking(false);
+        reactPlayerRef.current.seekTo(played);
     }
 
     return (
@@ -48,12 +65,24 @@ function SongPlayer() {
                         </Typography>
                     </div>
                     <Slider
+                        onMouseDown={handleSeekMouseDown}
+                        onMouseUp={handleSeekMouseUp}
+                        onChange={handleProgressChange}
+                        value={played}
                         type="range"
                         min={0}
                         max={1}
                         step={0.01}
                     />
                 </div>
+                <ReactPlayer
+                    ref={reactPlayerRef}
+                    onProgress={({ played, playedSeconds }) => {
+                        setPlayed(played)
+                    }}
+                    url={state.song.url}
+                    playing={state.isPlaying}
+                    hidden />
                 <CardMedia
                     className='thumbnail-songplayer'
                     image={state.song.thumbnail} />
