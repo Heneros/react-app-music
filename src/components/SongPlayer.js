a33f524a5d03c2bcbace3ac10f20ea83f2d9b116
Heneros <punk60826@gmail.com>
2022-09-25 01:01:01 +0300

    prev and next for song added

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -12,11 +12,26 @@ function SongPlayer() {
     const { data } = useQuery(GET_QUEUED_SONGS);
     const { state, dispatch } = React.useContext(SongContext);
     const [played, setPlayed] = React.useState(0);
+    const [playedSeconds, setPlayedSeconds] = React.useState(0);
     const reactPlayerRef = React.useRef();
-
-
     const [seeking, setSeeking] = React.useState(false);
+    const [positionInQueue, setPositionInQueue] = React.useState(0);
 
+
+    //Find current song id
+    React.useEffect(() => {
+        const songIndex = data.queue.findIndex(song => song.id === state.song.id);
+        setPositionInQueue(songIndex)
+    }, [data.queue, state.song.id]); //data.queue dependency useEffect
+
+
+    React.useEffect(() => {
+        const nextSong = data.queue[positionInQueue + 1];
+        if (played >= 0.99 && nextSong) {
+            setPlayed(0)
+            dispatch({ type: "SET_SONG", payload: { song: nextSong } })
+        }
+    }, [data.queue, played, dispatch, positionInQueue])
 
 
     function handleTogglePlay() {
@@ -29,12 +44,30 @@ function SongPlayer() {
 
     function handleSeekMouseDown() {
         setSeeking(true);
-
     }
 
     function handleSeekMouseUp() {
         setSeeking(false);
         reactPlayerRef.current.seekTo(played);
+    }
+
+    function formatDuration(seconds) {
+
+        return new Date(seconds * 1000).toISOString().substr(11, 8);
+    }
+
+    function handlePlayPrevSong() {
+        const prevSong = data.queue[positionInQueue - 1];
+        if (prevSong) {
+            dispatch({ type: "SET_SONG", payload: { song: prevSong } })
+        }
+    }
+
+    function handlePlayNextSong() {
+        const nextSong = data.queue[positionInQueue + 1];
+        if (nextSong) {
+            dispatch({ type: "SET_SONG", payload: { song: nextSong } })
+        }
     }
 
     return (
@@ -51,17 +84,18 @@ function SongPlayer() {
                         </Typography>
                     </CardContent>
                     <div className='controls-songplayer'>
-                        <IconButton>
+                        <IconButton onClick={handlePlayPrevSong}>
                             <SkipPrevious />
                         </IconButton>
                         <IconButton onClick={handleTogglePlay}>
                             {state.isPlaying ? <Pause className='playicon-songplayer' /> : <PlayArrow className='playicon-songplayer' />}
                         </IconButton>
-                        <IconButton>
+                        <IconButton onClick={handlePlayNextSong}>
                             <SkipNext />
                         </IconButton>
                         <Typography variant="subtitle1" component="p" color="textSecondary">
-                            00:01:30
+                            {formatDuration(playedSeconds)}
+                            {/* {playedSeconds} */}
                         </Typography>
                     </div>
                     <Slider
@@ -78,7 +112,10 @@ function SongPlayer() {
                 <ReactPlayer
                     ref={reactPlayerRef}
                     onProgress={({ played, playedSeconds }) => {
-                        setPlayed(played)
+                        if (!seeking) {
+                            setPlayed(played)
+                            setPlayedSeconds(playedSeconds);
+                        }
                     }}
                     url={state.song.url}
                     playing={state.isPlaying}
